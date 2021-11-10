4e135911d61667e6affa4837d0f66d473ce10416
Fredy Álvarez <fre.alvarezb@gmail.com>
2021-11-10 18:53:16 -0500

    Adding functionality to ProductList

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -2,46 +2,61 @@ import React from 'react';
 import { MdDeleteOutline, MdOutlineAdd, MdOutlineRemove } from 'react-icons/md';
 
 import './styles.css';
+import { moneyFormatter } from '../../common/utils';
 
-const MOCK_DATA = [
-  'something',
-  'milk',
-  'something',
-  'something',
-  'something',
-  'something',
-  'milk',
-  'milk',
-  'bread',
-  'bread',
-  'milk',
-  'bread',
-  'milk',
-  'something',
-  'milk',
-  'milk',
-  'bread',
-  'milk',
-  'milk',
+interface Product {
+  name: string;
+  quantity: number;
+  cost: number;
+  uniqueId: string;
+}
+
+const MOCK_DATA: Product[] = [
+  { name: 'Milk', quantity: 3, cost: 3.97, uniqueId: 'milk' },
+  { name: 'Bread', quantity: 3, cost: 2.17, uniqueId: 'bread' },
+  { name: 'Banana', quantity: 3, cost: 0.99, uniqueId: 'banana' },
+  { name: 'Apple', quantity: 3, cost: 0.89, uniqueId: 'apple' },
 ];
 
-function ProductItem() {
+interface ProductItemProps {
+  product: Product;
+}
+
+function ProductItem({ product }: ProductItemProps) {
+  const { cost, quantity, uniqueId, name } = product;
+  const formattedCost = moneyFormatter.format(quantity * cost);
+
+  const handleIncrease = () => {
+    //  TODO: do something with uniqueId
+    console.debug(`Increasing ${uniqueId} in 1`);
+  };
+
+  const handleDecrease = () => {
+    //  TODO: do something with uniqueId
+    console.debug(`Decreasing ${uniqueId} in 1`);
+  };
+
+  const handleRemove = () => {
+    //  TODO: do something with uniqueId
+    console.debug(`Removing ${uniqueId} from list`);
+  };
+
   return (
     <li className="productList-item">
       <div className="productList-item-description">
-        <span>something</span>
+        <span>{name}</span>
 
-        <span className="productList-item-quantity">x19</span>
+        <span className="productList-item-quantity">x{quantity}</span>
       </div>
-      <span className="productList-item-price">$12</span>
+      <span className="productList-item-price">{formattedCost}</span>
 
-      <button className="productList-item-button">
+      <button onClick={handleIncrease} className="productList-item-button">
         <MdOutlineRemove size={20} />
       </button>
-      <button className="productList-item-button">
+      <button onClick={handleDecrease} className="productList-item-button">
         <MdOutlineAdd size={20} />
       </button>
-      <button className="productList-item-button">
+      <button onClick={handleRemove} className="productList-item-button">
         <MdDeleteOutline color="red" size={20} />
       </button>
     </li>
@@ -51,8 +66,8 @@ function ProductItem() {
 function ProductList() {
   return (
     <ul className="productList-container">
-      {MOCK_DATA.map(() => (
-        <ProductItem />
+      {MOCK_DATA.map((v) => (
+        <ProductItem product={v} key={v.uniqueId} />
       ))}
     </ul>
   );
